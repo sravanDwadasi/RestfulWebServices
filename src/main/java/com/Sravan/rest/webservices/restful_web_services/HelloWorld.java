124a2e46c63a483d86780e049b1a808a4bee20e4
sravanDwadasi <sravandwadasi@gmail.com>
2025-04-28 12:44:21 +0530

    Added Restful Web Services

--- src/main/java/com/Sravan/rest/webservices/restful_web_services/HelloWorld.java
+++ src/main/java/com/Sravan/rest/webservices/restful_web_services/HelloWorld.java
@@ -1,4 +1,14 @@
 package com.Sravan.rest.webservices.restful_web_services;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorld {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    public String helloWorld(){
+    return "Hello World";
+    }
 }
